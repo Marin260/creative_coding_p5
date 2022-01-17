e01d5f70e7e5ec8b21a2017b0925677da22fbb0d
Marin260 <marin.martuslovic@gmail.com>
2022-01-17 11:27:17 +0100

    first commit

---
+++ js/example1.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight); // setting the canvas to the screen size
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight); // auto resize as the window resizes
+}
+
+function draw() {
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
+}
